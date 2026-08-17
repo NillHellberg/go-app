67d3cf42db4886c96276dd1b7fd3bbfac9c295ad
NillHellberg <nill@hellzerger.ru>
2026-08-17 14:19:13 +0300

    Fix syntax errors in Add function

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
-func Add(a, b, int) int {
+// Функция принимает два int (a и b) и возвращает int
+func Add(a int, b int) int {
 	return a + b
 }
 
-func main () {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, CI/CD World! 2+3=%d", Add(2, 3))
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Передаем ровно два аргумента: 2 и 3
+		fmt.Fprintf(w, "Hello, CI/CD World! 2+3=%d", Add(2, 3))
 	})
 
 	fmt.Println("Server starting on port 8080...")
@@ -19,3 +21,4 @@ func main () {
 		panic(err)
 	}
 }
+
